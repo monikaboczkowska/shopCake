4bc53b378d6717acf96387f9845fdc1517b9b625
Monika <boczkowska.monika@gmail.com>
2021-05-16 14:51:08 +0100

    make it better

--- app.js
+++ app.js
@@ -97,4 +97,19 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
+
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server started on port 3000");
+});
+//module.exports = app;
